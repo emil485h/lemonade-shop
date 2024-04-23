cf478a3932eb1b653c7d1954da01a802abb3b14a
KatarinaEgebjerg <katarina.egebjerg@gmail.com>
2024-04-23 12:41:17 +0200

    Laravel API

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,20 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './ProductList.css';
 
-const ProductList = ({ products, onAddToCart }) => {
+const ProductList = ({ onAddToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch products
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/products');
+        setProducts(response.data.products); // Assume response.data contains the array of products
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
+    <>
     <div className="product-list">
       {products.map((product) => (
         <div key={product.id} className="product-item">
-          
-          <img src={product.imageUrl} alt={product.name} className="product-image" />
-          <h3>{product.name}</h3>
+          <img src={product.image} alt={product.product} className="product-image" />
+          <h3>{product.product}</h3>
+          <p>{product.description}</p>
           <p>Price: {product.price} Kr.</p>
           <button onClick={() => onAddToCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
+    </>
   );
 };
-  
 
 export default ProductList;
