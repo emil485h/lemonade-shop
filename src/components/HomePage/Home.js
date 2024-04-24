08217bd32d4fbcb34c024576762e9cf7a044c575
emil485h <emil485h@gmail.com>
2024-04-24 13:16:52 +0200

    HomePage styling + Nav

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -2,10 +2,24 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Velkommen til Lemonade Shop</h1>
-      <p>Dette er hjemmesiden for vores butik!</p>
-    </div>
+    <body>
+      <div>
+        <h1>Velkommen til Lemonade Shop</h1>
+        <p>Dette er hjemmesiden for vores butik!</p>
+      </div>
+
+      <section>
+        <p>
+          Reprehenderit exercitation eu reprehenderit ut est ea occaecat aliqua
+          id eu laboris esse enim. Sit ex ullamco mollit nostrud aliqua tempor.
+          Ullamco proident nostrud sit nulla tempor. Culpa dolore veniam nulla
+          esse minim amet consequat quis labore voluptate aliquip consectetur
+          eiusmod. Quis nisi dolore aute nulla laboris deserunt proident aute ad
+          magna commodo ipsum in laboris.
+        </p>
+        <img src="Gin-Hass.webp"></img>
+      </section>
+    </body>
   );
 };
 
